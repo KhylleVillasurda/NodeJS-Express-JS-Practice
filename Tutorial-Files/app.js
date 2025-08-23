edfee56e6139ca073d978ab272fc3ffb3998eceb
KhylleVillasurda <freakyvader@proton.me>
2025-08-23 12:34:33 +0800

    Starting Node JS

---
+++ Tutorial-Files/app.js
@@ -0,0 +1,9 @@
+const amount = new String ("I'm a string") ;
+
+if (amount == 10) {
+    console.log("Excelleny");
+} else if (amount < 10) {
+    console.log("Please try again");
+} else {
+    console.log("Sir, Tapos na po");
+}
