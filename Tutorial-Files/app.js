3242e3f3e8d69aaa7c88158a9b6ef6f1c748c032
KhylleVillasurda <freakyvader@proton.me>
2025-08-29 14:07:43 +0800

    Practicing Basics Part 1

--- Tutorial-Files/app.js
+++ Tutorial-Files/app.js
@@ -1,7 +1,61 @@
-const numbers = [10, 20, 30, 40, 50];
+const fs = require('fs')
+const http = require("http")
+const path = require("path")
+const os = require("os")
+const url = require("url")
+const sayHello = require("./greetings")
 
-number[2] = "Thirty";
+const message = sayHello('Khylle')
+console.log(message);
 
-for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
-}
+
+
+
+
+/*
+const myUrl = new URL('https://example.com:8000/path/name?query=hello#hash')
+console.log("Host", myUrl.host);
+console.log("Pathname", myUrl.pathname);
+console.log("Search params", myUrl.searchParams.get("query")); 
+
+
+console.log("Platform:", os.platform());
+console.log("CPU architecture:", os.arch());
+console.log("Total memory:", os.totalmem());
+console.log("Free memory:", os.freemem());
+
+
+
+const directory = "/user/local"
+const fileName = "test.txt"
+
+const fullPath = path.join(directory, fileName)
+
+console.log(fullPath)
+//
+const server = http.createServer((req,res) =>{
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end("Ended")
+})
+
+server.listen(3000, () => {
+    console.log("Server running at http://localhost:3000/")
+})
+
+fs.readFile('test.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error('Error reading file:', err)
+    return;
+  }
+  console.log('File contents:', data); })
+
+   const content = "hello Node.js"
+ fs.writeFile("output.txt", content, err => {
+   if (err) {
+     console.error('Error writing file:', err)
+     return;
+   }
+   console.log('File written successfully');
+})
+  */
